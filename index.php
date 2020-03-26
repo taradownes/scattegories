ebe6d873f5ddce698793665bd15398b6ff2f3de0
Tara Downes <38165052+taradownes@users.noreply.github.com>
2020-03-26 18:36:12 -0500

    created php doc

---
+++ index.php
@@ -0,0 +1,303 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Scattegories</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Exo|Raleway|Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<div class="main">
+
+    <div class="header">
+        <div class="header_main">
+            <img src="scattegories.jpg">
+            <h1>To help us survive the Quarantine!!</h1>
+        </div>
+    </div>
+    <div class="dice-container">
+        <div class="dice">
+            <h1 class="random-letter">A</h1>
+        </div>
+    </div>
+    <div class="content">
+        <div class="card">
+            <div class="number">
+                <h1 class="1">1</h1>
+            </div>
+            <div class="card_content" id="1">
+                <ol class="card_list">
+                    <li>Things You Make</li>
+                    <li>Footwear</li>
+                    <li>Weapons</li>
+                    <li>Things That Have Spots</li>
+                    <li>Magazines</li>
+                    <li>Dairy Products</li>
+                    <li>Software</li>
+                    <li>Honeymoon Spots </li>
+                    <li>Things That Can Kill You </li>
+                    <li>Items in This Room</li>
+                    <li>Things That Can Get You Fired </li>
+                    <li>Farm Animals</li>
+                </ol>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="number">
+                <h1 class="2">2</h1>
+            </div>
+            <div class="card_content" id="2">
+                <ol class="card_list">
+                    <li>Celebrities</li>
+                    <li>Tourist Attractions</li>
+                    <li>Villians</li>
+                    <li>Items in a Catalog</li>
+                    <li>Things Made of Metal</li>
+                    <li>Things Found in a Desk</li>
+                    <li>Types of Drinks</li>
+                    <li>Insects</li>
+                    <li>Tools</li>
+                    <li>Crimes</li>
+                    <li>Words Associated With Money</li>
+                    <li>Something You're Afraid Of</li>
+                </ol>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="number">
+                <h1 class="3">3</h1>
+            </div>
+            <div class="card_content" id="3">
+                <ol class="card_list">
+                    <li>Math Terms</li>
+                    <li>Flowers</li>
+                    <li>Sports Played Indoors</li>
+                    <li>Names Used in the Bible</li>
+                    <li>Countries</li>
+                    <li>Games</li>
+                    <li>Famous Duos and Trios</li>
+                    <li>Cars</li>
+                    <li>School Supplies</li>
+                    <li>Bodies of Water</li>
+                    <li>Beers</li>
+                    <li>Things That Grow</li>
+                </ol>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="number">
+                <h1 class="4">4</h1>
+            </div>
+            <div class="card_content" id="4">
+                <ol class="card_list">
+                    <li>Items You Take on a Trip</li>
+                    <li>Things in a Grocery Store</li>
+                    <li>Things Found on a Map</li>
+                    <li>Titles People Can Have</li>
+                    <li>Places to Hang Out</li>
+                    <li>Sports Equipment</li>
+                    <li>Things at a Circus</li>
+                    <li>Parks</li>
+                    <li>Desserts</li>
+                    <li>Weekend Activities</li>
+                    <li>Pro Sports Teams</li>
+                    <li>Notorious People</li>
+                </ol>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="number" >
+                <h1 class="5">5</h1>
+            </div>
+            <div class="card_content" id="5">
+                <ol class="card_list">
+                    <li>Song Titles</li>
+                    <li>Fish</li>
+                    <li>Animal Noises</li>
+                    <li>Internet Lingo</li>
+                    <li>Nicknames</li>
+                    <li>A Girl's Name</li>
+                    <li>Weekend Activities</li>
+                    <li>Things in a Souvenir Shop</li>
+                    <li>Words Associated With Winter</li>
+                    <li>Pro Sports Teams</li>
+                    <li>Food/Drink That Is Green</li>
+                    <li>Reasons to Take Out a Loan</li>
+                </ol>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="number" >
+                <h1 class="6">6</h1>
+            </div>
+            <div class="card_content" id="6">
+                <ol class="card_list">
+                    <li>Words With Double Letters</li>
+                    <li>Personality Traits</li>
+                    <li>Things You See at the Zoo</li>
+                    <li>Things You Get in the Mail</li>
+                    <li>Musical Groups</li>
+                    <li>Childrens Books</li>
+                    <li>Words Associated With Money</li>
+                    <li>Insects</li>
+                    <li>Things Found on a Map</li>
+                    <li>Bad Habits</li>
+                    <li>Television Stars</li>
+                    <li>Things That Can Get You Fired</li>
+                </ol>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="number" >
+                <h1 class="7">7</h1>
+            </div>
+            <div class="card_content" id="7">
+                <ol class="card_list">
+                    <li>Vehicles</li>
+                    <li>Parks</li>
+                    <li>Sports</li>
+                    <li>Ways to Kill Time</li>
+                    <li>Ice Cream Flavors</li>
+                    <li>Presidents</li>
+                    <li>Movie Titles</li>
+                    <li>Things Made of Metal</li>
+                    <li>Things That Have Spots</li>
+                    <li>Footwear</li>
+                    <li>Household Chores</li>
+                    <li>Olympic Events</li>
+                </ol>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="number" >
+                <h1 class="8">8</h1>
+            </div>
+            <div class="card_content" id="8">
+                <ol class="card_list">
+                    <li>Something You're Afraid Of</li>
+                    <li>Languages</li>
+                    <li>Words Ending in -N</li>
+                    <li>Tools</li>
+                    <li>Acronyms</li>
+                    <li>Game Terms</li>
+                    <li>Things That Use a Remote</li>
+                    <li>Baby Foods</li>
+                    <li>World Records</li>
+                    <li>World Records</li>
+                    <li>World Records</li>
+                    <li>Things in a Grocery Store</li>
+                </ol>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="number" >
+                <h1 class="9">9</h1>
+            </div>
+            <div class="card_content" id="9">
+                <ol class="card_list">
+                    <li>Things That You Wear</li>
+                    <li>Fruits</li>
+                    <li>Famous Duos and Trios</li>
+                    <li>Reasons to Call 911</li>
+                    <li>Reptiles/Amphibians</li>
+                    <li>Games</li>
+                    <li>Things You Store Items In</li>
+                    <li>Vacation Spots</li>
+                    <li>Things to Do at a Party</li>
+                    <li>Types of Drink</li>
+                    <li>Furniture</li>
+                    <li>Things You Shouldn't Touch</li>
+                </ol>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="number" >
+                <h1 class="10">10</h1>
+            </div>
+            <div class="card_content" id="10">
+                <ol class="card_list">
+                    <li>Weapons</li>
+                    <li>Parts of the Body</li>
+                    <li>Animals in Books or Movies</li>
+                    <li>Things With Tails</li>
+                    <li>States</li>
+                    <li>Items in a Suitcase</li>
+                    <li>Offensive Words</li>
+                    <li>Gifts/Presents</li>
+                    <li>Things at a Carnival</li>
+                    <li>Things That Make You Smile</li>
+                    <li>Things That Are Black</li>
+                    <li>Computer Parts</li>
+                </ol>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="number" >
+                <h1 class="11">11</h1>
+            </div>
+            <div class="card_content" id="11">
+                <ol class="card_list">
+                    <li>Titles People Can Have</li>
+                    <li>Heroes</li>
+                    <li>Names Used in Songs</li>
+                    <li>Things You Sit In/On</li>
+                    <li>Items in a Catalog</li>
+                    <li>Items in a Kitchen</li>
+                    <li>Ways to Get From Here to There</li>
+                    <li>Tourist Attractions</li>
+                    <li>Birds</li>
+                    <li>Things in the Sky</li>
+                    <li>Personality Traits</li>
+                    <li>Something You Keep Hidden</li>
+                </ol>
+            </div>
+
+        </div>
+        <div class="card">
+            <div class="number" >
+                <h1 class="12">12</h1>
+            </div>
+            <div class="card_content" id="12">
+                <ol class="card_list">
+                    <li>Things Found in a Desk</li>
+                    <li>Car Parts</li>
+                    <li>Flowers</li>
+                    <li>Bodies of Water</li>
+                    <li>Reasons to Quit Your Job</li>
+                    <li>Things You're Allergic To</li>
+                    <li>Countries</li>
+                    <li>Stones/Gems</li>
+                    <li>Villians</li>
+                    <li>Something You're Afraid Of</li>
+                    <li>Words Associated With Exercise</li>
+                    <li>U.S. Cities</li>
+                </ol>
+            </div>
+
+        </div>
+    </div>
+
+
+
+</div>
+
+
+</body>
+<script
+        src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous"></script>
+<script src="style.js"></script>
+</html>
